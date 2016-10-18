1ede0ea6d75d8512abce5c5be5ff34401d8f572b
brucechen1019 <lin.chen1019@hotmail.com>
2016-10-18 14:25:43 +0800

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -97,12 +97,14 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
 echo Handling .NET Web Application deployment.
 echo hello world
 
+echo Restore NuGet packages
 :: 1. Restore NuGet packages
 IF /I "AspDotNet-MVC-TypeScript.sln" NEQ "" (
   call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\AspDotNet-MVC-TypeScript.sln"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
+echo Build to the temporary path
 :: 2. Build to the temporary path
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApplication\WebApplication.csproj" /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
@@ -112,6 +114,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 
 IF !ERRORLEVEL! NEQ 0 goto error
 
+echo KuduSync
 :: 3. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
